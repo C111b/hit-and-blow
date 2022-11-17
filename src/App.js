ba880ace2b27b06943df4a50f8abe10161a8e39f
Caley <caley.badillo@gmail.com>
2022-11-17 13:14:01 -0500

    deployed?

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Game from "./Game.js";
 import {
   CssBaseline,
 } from "@mui/material";
-
+import { HashRouter, Route, Switch } from "react-router-dom";
 import ColorModeContextProvider from "./ColorModeContextProvider.js";
 // const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 const App= () => {
   return (
     <>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <ColorModeContextProvider>
         <CssBaseline />
         <Game />
       </ColorModeContextProvider>
+      </HashRouter>
     </>
   );
 }
